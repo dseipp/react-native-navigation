42c4629d7a33016cc71bac41049111f5e26bf9bf
dseipp <david@seipp.us>
2018-02-12 13:35:13 -0500

    remove back button and handle it a different way

--- src/components/root.js
+++ src/components/root.js
@@ -1,38 +1,12 @@
 import React, {Component} from 'react';
-import {BackHandler} from 'react-native';
 import {connect} from 'react-redux';
-import autoBind from 'class-autobind';
 import {addNavigationHelpers} from 'react-navigation';
-import {back} from '../actions';
 
 import {navigationStateSelector, navigationTypeSelector} from '../selectors';
 import {getNavigator} from '../services';
 
 // the container component
 class Root extends Component {
-
-  constructor() {
-    super();
-    autoBind(this);
-  }
-
-  componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
-  }
-
-  componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
-  }
-
-  onBackPress() {
-    const {dispatch, nav} = this.props;
-    if (nav.index === 0) {
-      return false;
-    }
-
-    dispatch(back());
-    return true;
-  }
 
   render() {
     const {navigationType, navigationState, dispatch} = this.props;
